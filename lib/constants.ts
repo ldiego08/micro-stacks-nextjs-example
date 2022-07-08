afabb5bec63242e0bc07ca18e19110e42ef8b0e9
aulneau <aulneau@users.noreply.github.com>
2022-07-08 11:19:40 -0500

    fix: url again

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,2 @@
-export const URL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
+const VERCEL_URL = process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
+export const URL = VERCEL_URL ? `https://${VERCEL_URL}` : 'http://localhost:3000';
