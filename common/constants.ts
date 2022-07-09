a9ad794015bf9b26084ff01cf4c6013025b45119
aulneau <aulneau@users.noreply.github.com>
2022-07-09 08:22:00 -0500

    fix: custom app env var

--- common/constants.ts
+++ common/constants.ts
@@ -1,2 +1,10 @@
+// this is something specific to this deployment,
+// VERCEL_URL will be the deployment url that vercel uses and ultimately proxies with any kind of domain
+// assignment, as such, the CORS requests will fail, even tho they are the same deployment.
+// I've set this value to https://nextjs-example.micro-stacks.dev as it's where this demo will live
+// in your own apps you should do something similar
+const CUSTOM_APP_URL_ENV_VAR = process.env.NEXT_PUBLIC_APP_URL;
 const VERCEL_URL = process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
-export const API_URL = VERCEL_URL ? `https://${VERCEL_URL}` : 'http://localhost:3000';
+
+export const APP_URL =
+  CUSTOM_APP_URL_ENV_VAR ?? VERCEL_URL ? `https://${VERCEL_URL}` : 'http://localhost:3000';
