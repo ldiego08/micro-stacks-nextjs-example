41110be1381b4be19088acea535a4dd59aacf6b1
aulneau <aulneau@users.noreply.github.com>
2022-07-09 08:51:40 -0500

    fix: more simple

--- common/constants.ts
+++ common/constants.ts
@@ -1,11 +1,9 @@
-// this is something specific to this deployment,
-// VERCEL_URL will be the deployment url that vercel uses and ultimately proxies with any kind of domain
-// assignment, as such, the CORS requests will fail, even tho they are the same deployment.
-// I've set this value to https://nextjs-example.micro-stacks.dev as it's where this demo will live
-// in your own apps you should do something similar
-const CUSTOM_APP_URL_ENV_VAR =
-  process.env.NODE_ENV === 'production' && 'https://nextjs-example.micro-stacks.dev';
-const VERCEL_URL = process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
+// this is something specific to this deployment, using something like NEXT_PUBLIC_VERCEL_URL will be the deployment
+// url that vercel uses and ultimately proxies with any kind of domain assignment to, as such, the CORS requests will
+// fail, even tho they are the same deployment. in your own apps you should do something similar if you know the fixed
+// domain this app will be deployed to
 
 export const APP_URL =
-  CUSTOM_APP_URL_ENV_VAR ?? VERCEL_URL ? `https://${VERCEL_URL}` : 'http://localhost:3000';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://nextjs-example.micro-stacks.dev';
